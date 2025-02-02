b43d24b25c25aa820e684087216b04828ee0c593
Ananya1812 <ananyakini04@gmail.com>
2025-02-02 19:52:44 +0530

    updated

--- 1/sortedPresentElement.py
+++ 1/sortedPresentElement.py
@@ -1,22 +1,10 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 def search(nums, target):
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            return mid
-        if nums[left] <= nums[mid]:
-            if nums[left] <= target < nums[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
-        else:
-            if nums[mid] < target <= nums[right]:
-                left = mid + 1
-            else:
-                right = mid - 1
+    for i in range(len(nums)):
+        if nums[i] == target:
+            return i
     return -1
 
+a,target=map(int,input().split())
 nums = list(map(int, input().split()))
-target = int(input())
-print(search(nums, target))
+print(search(nums,target))
